cfb085366003ca5777038e9fb2c21cb862fa4388
Sonu <sbamniya23@gmail.com>
2026-09-06 16:08:36 +0530

    chore: update dependencies and scripts in package.json
    - Added class-variance-authority, clsx, nativewind, tailwind-merge, tailwindcss, tailwindcss-animate, and uniwind to dependencies.
    - Updated @babel/core in devDependencies.
    - Changed start scripts to use expo run:android and expo run:ios.

--- youse-app/app/_layout.tsx
+++ youse-app/app/_layout.tsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/components/useColorScheme';
+import '../global.css';
 
 export {
   // Catch any errors thrown by the Layout component.
